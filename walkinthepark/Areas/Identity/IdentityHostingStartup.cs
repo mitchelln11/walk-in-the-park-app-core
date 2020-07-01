b0ca5e29d60b77c818910d0baa91c4f0382c5fe2
Mitchell Naleid <mitchellnaleid@gmail.com>
2020-07-01 10:43:44 -0500

    reverted styles, added identity pages. (Didn't know you could before), updated identity pages.

---
+++ walkinthepark/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using walkinthepark.Data;
+
+[assembly: HostingStartup(typeof(walkinthepark.Areas.Identity.IdentityHostingStartup))]
+namespace walkinthepark.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
